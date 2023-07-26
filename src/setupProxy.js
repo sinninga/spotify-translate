9a685553e63f794f2b3e14317e2a60b3e42dae5a
Adam Sinning <asinning25@gmail.com>
2023-07-25 23:54:37 -0600

    proxy configuration

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,5 @@
-// module.exports = function (app) {
-//     app.use(proxy(`/auth/**`, { 
-//         target: 'http://localhost:5000' 
-//     }));
-// };
+module.exports = function (app) {
+    app.use(proxy(`/auth/**`, { 
+        target: 'http://localhost:5000' 
+    }));
+};
